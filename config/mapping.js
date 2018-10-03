e4f26ad71895d59aa4ddfd421004226102f383d3
ludovic <wulfy72@yahoo.fr>
2018-10-03 20:36:47 +0000

    add selector switch (#5)
    * add selector switch
    
    * remove useless comment

--- config/mapping.js
+++ config/mapping.js
@@ -149,5 +149,47 @@ const DOMOTICZ_ALEXA_CONTACT_EXAMPLE = {
 	]
 };
 
+const DOMOTICZ_ALEXA_SELECTOR_MAPPING = {
+	"domoticz_mapping" : {
+		"Type":"Light/Switch",
+		"Subtype": "Selector Switch"
+	},
+	"discovery" : {
+		...COMMON_DISCOVERY_MAPPING,
+		"displayCategories" : ["LIGHT"],
+		"cookie": {
+		},
+	},
+	"capabilities" : [
+		{
+			"interface":"Alexa.PercentageController",
+			"state":{
+				"percentage":"@Level@",
+			},
+			"command":{
+			},
+			"supported": [{
+	            "name": "percentage",
+	            "value" : "@Level@",
+	        }],
+	        "retrievable": true
+		},
+		{
+			"interface":"Alexa.PowerController",
+			"state":{
+				"powerState":"ON",
+			},
+			"command":{
+			},
+			"supported": [{
+	            "name": "powerState",
+	            "value": "()=>@Level@ == 100 ? 'OFF' : 'ON'",
+	        }],
+	        "proactivelyReported": true,
+	        "retrievable": true
+		}
+	]
+};
 
-exports.ALEXAMAPPING = [DOMOTICZ_ALEXA_VOLET_EXAMPLE,DOMOTICZ_ALEXA_PRISE_EXAMPLE,DOMOTICZ_ALEXA_TEMP_EXAMPLE];
+
+exports.ALEXAMAPPING = [DOMOTICZ_ALEXA_VOLET_EXAMPLE,DOMOTICZ_ALEXA_PRISE_EXAMPLE,DOMOTICZ_ALEXA_TEMP_EXAMPLE,DOMOTICZ_ALEXA_SELECTOR_MAPPING];

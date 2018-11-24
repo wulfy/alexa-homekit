3e37cfc2e5a63e815b866aac7268212b60a5eaac
ludovic <wulfy72@yahoo.fr>
2018-11-24 23:11:29 +0100

    move some code in separated objects

--- domoticz.js
+++ domoticz.js
@@ -9,6 +9,7 @@ const {promiseHttpRequest} = require('./config/httpUtils');
 const PROD_MODE = process.env.PROD_MODE === "true";
 const {sendStatsd} = require('./config/metrics');
 
+
 class domoticz {
 
 	constructor(token){
@@ -32,24 +33,10 @@ class domoticz {
 		}
 	}
 
+
 	//extract domoticz url data ex: http://my.domoti.cz 
 	//to retrieve proto (HTTP/HTTPS) and use correct node http or https lib
 	// and domain my.domoti.cz
-	extractDomoticzUrlData (request) {
-	  let domoticzUrlData = {domain:null,proto:"HTTP"};
-	  const result = request.split("//").map((value)=>value.split(":")[0]);
-	  console.log(result)
-	  if(result.length > 1)
-	  {
-	      domoticzUrlData.proto = result[0];
-	      domoticzUrlData.domain = result[1];
-	  }else{
-	      domoticzUrlData.domain = result[0];
-	  }
-
-	  return domoticzUrlData;
-	}
-
 	//request devices using a filter example: &rid=2
 	async requestDomoticzWithFilter (filter) {
 		if(! this.token)
@@ -58,6 +45,7 @@ class domoticz {
 		console.log("get devices original");
 		const base = await this.getBase();
 		const request = base+"?"+LIST_DEVICE_REQUEST + filter;
+
 		console.log("getDevices " + request);
 		const devicesJsonList = await promiseHttpRequest(request);
 		console.log(devicesJsonList)
@@ -82,12 +70,15 @@ class domoticz {
 		let deviceRequest = base + "?" + SET_COMMAND;
 		//const params = overrideParams && typeof overrideParams === "function" ? overrideParams(requestMethod) : DEVICE_HANDLER_COMMANDS_PARAMS[requestMethod];
 		deviceRequest += generate_command(deviceSubtype,deviceId,directive,directiveValue);
+
 		console.log(deviceRequest);
 		try {
 			PROD_MODE ? await promiseHttpRequest(deviceRequest) : null ;
 			console.log("REQUEST SENT");
 			sendStatsd("calls.command."+deviceSubtype+":1|c");
+
 			return deviceRequest;
+
 		}catch(e){
 			throw e;
 		}

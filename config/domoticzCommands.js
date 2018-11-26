8b31c4f891244a219523cc49aefc35ef17cb36f6
ludovic <wulfy72@yahoo.fr>
2018-11-26 23:29:11 +0100

    add venitian stop command

--- config/domoticzCommands.js
+++ config/domoticzCommands.js
@@ -3,14 +3,20 @@ const SET_DEVICE_LVL = "switchcmd=Set%20Level"
 const PERCENT_VALUE = "level";
 const SET_DEVICE_ON = "switchcmd=On"
 const SET_DEVICE_OFF = "switchcmd=Off"
+const SET_VOLET_STOP = "switchcmd=Stop"
 exports.STATE_REQUEST = "type=devices"
 exports.SET_COMMAND = "type=command&param=switchlight"
 const VOLET_SUBTYPE = "BlindsPercentage";
+const VOLET_VENETIAN_SUBTYPE = "Venetian Blinds US";
 
-exports.device_handler_command = (subType)=>({
+device_handler_command = (subType,value)=>({
 	"SetPercentage": {
-		"command" : SET_DEVICE_LVL,
-		"value" : PERCENT_VALUE,
+		"command" : subType === VOLET_VENETIAN_SUBTYPE && 50 === value 
+					? SET_VOLET_STOP
+					: SET_DEVICE_LVL,
+		"value" : subType === VOLET_VENETIAN_SUBTYPE && 50 === value 
+					? PERCENT_VALUE + "=" + 0
+					: PERCENT_VALUE + "=" + value,
 	},
 	"TurnOff": {
 		"command" : subType === VOLET_SUBTYPE ? SET_DEVICE_ON : SET_DEVICE_OFF,
@@ -19,3 +25,11 @@ exports.device_handler_command = (subType)=>({
 		"command" : subType === VOLET_SUBTYPE ? SET_DEVICE_OFF : SET_DEVICE_ON,
 	},
 });
+
+exports.generate_command = (subtype,command,value) => {
+	const paramsMapper = device_handler_command(subtype,value)[command];
+	deviceRequest += `&idx=${deviceId}&${paramsMapper["command"]}`;
+
+	if(paramsMapper["value"])
+		deviceRequest += `&${paramsMapper["value"]}=${deviceCommandValue}`
+}
--- domoticzApiHelper.js
+++ domoticzApiHelper.js
@@ -8,7 +8,7 @@ const https = require('https');
 const { LIST_DEVICE_REQUEST, 
 		STATE_REQUEST, 
 		SET_COMMAND, 
-		device_handler_command
+		generate_command
 	} = require("./config/domoticzCommands")
 const { DOMOTICZ_ALEXA_DISCOVERY_MAPPING, 
 		ALEXAMAPPING
@@ -306,13 +306,8 @@ exports.sendDeviceCommand = async function (request, value){
 	const deviceId = request.directive.endpoint.endpointId.split("_")[0];
 	const subtype = request.directive.endpoint.endpointId.split("_")[2];
 	//const params = overrideParams && typeof overrideParams === "function" ? overrideParams(requestMethod) : DEVICE_HANDLER_COMMANDS_PARAMS[requestMethod];
-	const paramsMapper =device_handler_command(subtype)[requestMethod];
-	deviceRequest += `&idx=${deviceId}&${paramsMapper["command"]}`;
+	deviceRequest += generate_command(subtype,requestMethod,deviceCommandValue);
 
-	if(deviceCommandValue && paramsMapper["value"])
-		deviceRequest += `&${paramsMapper["value"]}=${deviceCommandValue}`
-
-	console.log(deviceRequest);
 	try {
 		PROD_MODE ? await promiseHttpRequest(deviceRequest) : null ;
 		console.log("REQUEST SENT");

75bb13f4b100dc2230e2a74e46dfd91ebd32c189
ludovic <wulfy72@yahoo.fr>
2018-12-29 21:44:30 +0100

    change way to get on/off power state , update tests and fix push on commands

--- test/__tests__/test_stateReport.js
+++ test/__tests__/test_stateReport.js
@@ -1,17 +1,55 @@
 const base_config = require("../config_tests/base_config");
 
-const { DOMOTICZ_GET_DEVICES } = require("../mockups/client4Mockup");
+test('GET DEVICE STATE VENITIAN BLIND', async done => {
 
-global.getDomoticzFromToken = (token) => {
-    return new base_config.mockedDomoticz(token,DOMOTICZ_GET_DEVICES);
-}
+    const { DOMOTICZ_GET_DEVICES } = require("../mockups/client4Mockup");
+    global.getDomoticzFromToken = (token) => {
+        return new base_config.mockedDomoticz(token,DOMOTICZ_GET_DEVICES);
+    }
 
-test('GET DEVICE STATE', async done => {
     const data = await base_config.getAlexaDeviceState("notoken","73_RFY_VenetianBlindsUS");
     //removing timestamp data for tests
     data.properties.forEach(property => property.timeOfSample = null);
     expect(data).toEqual({"properties":
                                     [
+                                        {
+                                            "name": "percentage", 
+                                            "namespace": "Alexa.PercentageController",
+                                            "timeOfSample":null, 
+                                            "uncertaintyInMilliseconds": 500, 
+                                            "value": "0"
+                                        },
+                                        {
+                                            "name": "powerState", 
+                                            "namespace": "Alexa.PowerController",
+                                            "timeOfSample":null, 
+                                            "uncertaintyInMilliseconds": 500, 
+                                            "value": "ON"
+                                        }, 
+                                    ]
+                    });
+    done();
+});
+
+test('GET DEVICE STATE BLINDS RFY', async done => {
+
+    const { DOMOTICZ_GET_DEVICES } = require("../mockups/client11Mockup");
+    global.getDomoticzFromToken = (token) => {
+        return new base_config.mockedDomoticz(token,DOMOTICZ_GET_DEVICES);
+    }
+
+    const data = await base_config.getAlexaDeviceState("notoken","2_ASA_Blinds");
+    //removing timestamp data for tests
+    data.properties.forEach(property => property.timeOfSample = null);
+    expect(data).toEqual({"properties":
+                                    [
+                                        {
+                                            "name": "percentage", 
+                                            "namespace": "Alexa.PercentageController",
+                                            "timeOfSample":null, 
+                                            "uncertaintyInMilliseconds": 500, 
+                                            "value": "0"
+                                        },
                                         {
                                             "name": "powerState", 
                                             "namespace": "Alexa.PowerController",
@@ -19,13 +57,130 @@ test('GET DEVICE STATE', async done => {
                                             "uncertaintyInMilliseconds": 500, 
                                             "value": "OFF"
                                         }, 
+                                    ]
+                    });
+    done();
+});
+
+test('GET DEVICE STATE BLINDS SWITCH', async done => {
+
+    const { DOMOTICZ_GET_DEVICES } = require("../mockups/client12Mockup");
+    global.getDomoticzFromToken = (token) => {
+        return new base_config.mockedDomoticz(token,DOMOTICZ_GET_DEVICES);
+    }
+
+    const data = await base_config.getAlexaDeviceState("notoken","34_Switch_Blinds");
+    //removing timestamp data for tests
+    data.properties.forEach(property => property.timeOfSample = null);
+    expect(data).toEqual({"properties":
+                                    [
                                         {
                                             "name": "percentage", 
                                             "namespace": "Alexa.PercentageController",
                                             "timeOfSample":null, 
                                             "uncertaintyInMilliseconds": 500, 
-                                            "value": "0"
-                                        }
+                                            "value": "100"
+                                        },
+                                        {
+                                            "name": "powerState", 
+                                            "namespace": "Alexa.PowerController",
+                                            "timeOfSample":null, 
+                                            "uncertaintyInMilliseconds": 500, 
+                                            "value": "OFF"
+                                        }, 
+                                    ]
+                    });
+    done();
+});
+
+test('GET DEVICE STATE BLINDS INVERTED', async done => {
+
+    const { DOMOTICZ_GET_DEVICES } = require("../mockups/client3Mockup");
+    global.getDomoticzFromToken = (token) => {
+        return new base_config.mockedDomoticz(token,DOMOTICZ_GET_DEVICES);
+    }
+
+    const data = await base_config.getAlexaDeviceState("notoken","185_AC_BlindsInverted");
+    //removing timestamp data for tests
+    data.properties.forEach(property => property.timeOfSample = null);
+    expect(data).toEqual({"properties":
+                                    [
+                                        {
+                                            "name": "percentage", 
+                                            "namespace": "Alexa.PercentageController",
+                                            "timeOfSample":null, 
+                                            "uncertaintyInMilliseconds": 500, 
+                                            "value": "100"
+                                        },
+                                        {
+                                            "name": "powerState", 
+                                            "namespace": "Alexa.PowerController",
+                                            "timeOfSample":null, 
+                                            "uncertaintyInMilliseconds": 500, 
+                                            "value": "ON"
+                                        }, 
+                                    ]
+                    });
+    done();
+});
+
+test('GET DEVICE STATE BLIND PERCENTAGE', async done => {
+
+    const { DOMOTICZ_GET_DEVICES } = require("../mockups/client10Mockup");
+    global.getDomoticzFromToken = (token) => {
+        return new base_config.mockedDomoticz(token,DOMOTICZ_GET_DEVICES);
+    }
+
+    const data = await base_config.getAlexaDeviceState("notoken","201_Switch_BlindsPercentage");
+    //removing timestamp data for tests
+    data.properties.forEach(property => property.timeOfSample = null);
+    expect(data).toEqual({"properties":
+                                    [
+                                        {
+                                            "name": "percentage", 
+                                            "namespace": "Alexa.PercentageController",
+                                            "timeOfSample":null, 
+                                            "uncertaintyInMilliseconds": 500, 
+                                            "value": "100"
+                                        },
+                                        {
+                                            "name": "powerState", 
+                                            "namespace": "Alexa.PowerController",
+                                            "timeOfSample":null, 
+                                            "uncertaintyInMilliseconds": 500, 
+                                            "value": "OFF"
+                                        }, 
+                                    ]
+                    });
+    done();
+});
+
+test('GET DEVICE STATE DIMMER', async done => {
+
+    const { DOMOTICZ_GET_DEVICES } = require("../mockups/client6Mockup");
+    global.getDomoticzFromToken = (token) => {
+        return new base_config.mockedDomoticz(token,DOMOTICZ_GET_DEVICES);
+    }
+
+    const data = await base_config.getAlexaDeviceState("notoken","271_Switch_Dimmer");
+    //removing timestamp data for tests
+    data.properties.forEach(property => property.timeOfSample = null);
+    expect(data).toEqual({"properties":
+                                    [
+                                        {
+                                            "name": "percentage", 
+                                            "namespace": "Alexa.PercentageController",
+                                            "timeOfSample":null, 
+                                            "uncertaintyInMilliseconds": 500, 
+                                            "value": "100"
+                                        },
+                                        {
+                                            "name": "powerState", 
+                                            "namespace": "Alexa.PowerController",
+                                            "timeOfSample":null, 
+                                            "uncertaintyInMilliseconds": 500, 
+                                            "value": "ON"
+                                        }, 
                                     ]
                     });
     done();

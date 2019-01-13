0789560223202cd0ae8d2447a32dd1fcf02dee3c
ludovic <wulfy72@yahoo.fr>
2019-01-13 22:12:35 +0100

    fix tests

--- config/mapping.js
+++ config/mapping.js
@@ -161,17 +161,10 @@ const DOMOTICZ_ALEXA_ON_OFF = {
 };
 
 const DOMOTICZ_ALEXA_PUSH_ON = {
+	...DOMOTICZ_ALEXA_ON_OFF,
 	"domoticz_mapping" : {
 		"Switchtype": "Push On Button"
-	},
-	"discovery" : {
-		...COMMON_DISCOVERY_MAPPING,
-		"displayCategories" : ["SWITCH"],
-		"cookie": {},
-	},
-	"capabilities" : [
-		COMMON_POWER_CONTROLLER_CAPABILITY
-	]
+	}
 };
 
 const DOMOTICZ_ALEXA_GROUP = {

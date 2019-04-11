812ab5c69f4d243f33acda1bde2c26416c0dcaf7
ludovic <wulfy72@yahoo.fr>
2019-04-11 23:05:25 +0200

    add char to prevent empty description

--- config/mapping.js
+++ config/mapping.js
@@ -2,7 +2,7 @@ const COMMON_DISCOVERY_MAPPING = {
 		"endpointId" : "@idx@_@HardwareName@_@SwitchType@",
 		"manufacturerName" : "@HardwareName@",
 		"friendlyName" : "@Name@",
-		"description" : "@Description@",
+		"description" : "Desc: @Description@",
 }
 
 const COMMON_SCENE_MAPPING_CAPABILITY = {

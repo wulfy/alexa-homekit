c169881081eba04c819f82533f108ed9b330fc6b
ludovic <wulfy72@yahoo.fr>
2019-04-16 22:51:16 +0200

    remove unsafe log from databaseLogger

--- config/database.js
+++ config/database.js
@@ -1,5 +1,5 @@
 const mysql = require( 'mysql' );
-const {databaseLogger} = require('./logger.js');
+const {databaseLogger, debugLogger} = require('./logger.js');
 const {DBCONFIG} = require('./constants')
 const {sendStatsd} = require('./metrics');
 
@@ -56,7 +56,7 @@ const getUserData = (token) => {
                                      LEFT JOIN user_data as ud ON ud.user_id = ot.user_id 
                                      WHERE ot.access_token = ? `, 
     [token]).then( results => {
-        databaseLogger(results[0])
+        debugLogger(results[0])
         let data = results[0];
         if(!data) 
             throw "ERROR NO TOKEN FOUND";

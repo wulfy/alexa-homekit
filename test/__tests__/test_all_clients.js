d9ad2cc08fe0dbe9c4f1a8ce9fc1cdfd6f99e908
ludovic <wulfy72@yahoo.fr>
2020-04-19 01:12:46 +0200

    fix tests \o/

--- test/__tests__/test_all_clients.js
+++ test/__tests__/test_all_clients.js
@@ -1,45 +1,19 @@
 const base_config = require("../config_tests/base_config");
 const {override} = require("../config_tests/functions_override");
 
-let file = '';
-let domoticz_mockup = '';
-
-const doTest = async _ => {
-	for(let id = 10; id < 25; id++){
-		let file = "../mockups/client"+id+"Mockup";
-		let domoticz_mockup = { DOMOTICZ_GET_DEVICES } = require(file);
-
-		console.log("test " + id);
-		let context = {};
-		override(domoticz_mockup.DOMOTICZ_GET_DEVICES);
-		context.succeed = function (data){
-			const testData = JSON.stringify(data); 
-			expect(testData).toMatchSnapshot();
-		};
-		await base_config.handler(base_config.mockups.ALEXA_DISCOVERY_REQUEST_EXAMPLE,context);
-	}
-	sleep(4000);
-}
-
-let testData = '';
-
-beforeEach(() => testData = '');
-
 describe('Testing all clients', () => { 
-	for(let id = 10; id < 25; id++){
-		it('Testing client id ' + id, async () => { 
+	for(let id = 1; id <= 27; id++){
+		it('Testing client id ' + id, () => { 
 			let file = "../mockups/client"+id+"Mockup";
 			let domoticz_mockup = { DOMOTICZ_GET_DEVICES } = require(file);
-
-			console.log("test " + id);
 			let context = {};
-			
+
 			override(domoticz_mockup.DOMOTICZ_GET_DEVICES);
 			context.succeed = (data) => {
 				testData = JSON.stringify(data);
+				expect(testData).toMatchSnapshot();
 			};
 			base_config.handler(base_config.mockups.ALEXA_DISCOVERY_REQUEST_EXAMPLE,context);
-			expect(testData).toMatchSnapshot();
 		});
 	}
 });

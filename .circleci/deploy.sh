1f9731e68c62fb14d63323b3f79940a43c095b9c
ludovic <wulfy72@yahoo.fr>
2024-02-05 20:16:42 +0100

    add S3 deploy

--- .circleci/deploy.sh
+++ .circleci/deploy.sh
@@ -14,5 +14,7 @@ fi
 zip -r "${current_build}.zip" *.js config node_modules
 echo "Checking if function $current_build already exists"
 aws lambda list-functions | jq -r --arg CURRENTFUNCTION "$current_build" '.Functions[] | select(.FunctionName==$CURRENTFUNCTION) | .FunctionArn'
+echo "Sending zip to S3"
+aws s3api put-object --bucket alhau --key "deploy/${current_build}.zip" --body "${current_build}.zip"
 echo "Updating function: $current_build"
-aws lambda update-function-code --function-name "$current_build" --zip-file fileb://./"${current_build}.zip" --no-publish
+aws lambda update-function-code --function-name "$current_build" --s3-bucket alhau --s3-key "deploy/${current_build}.zip" --region eu-west-1 --no-publish

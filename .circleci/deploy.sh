9785ff640d855663cd2d74797e12dd23ff0b6ee1
ludovic <wulfy72@yahoo.fr>
2024-03-24 20:07:46 +0100

    Config circle (#264)
    * update calls to new Domoticz api format (2023.2)
    
    * fix config override and better tests
    
    * config circle
    
    * config circle
    
    * config circle
    
    * config circle
    
    * config circle
    
    * config circle
    
    * config circle
    
    * config circle
    
    * config circle
    
    * config circle
    
    * config circle
    
    * config circle
    
    * config circle
    
    * config circle
    
    * config circle
    
    * config circle
    
    * config circle
    
    * config circle
    
    * config circle
    
    * config circle
    
    * config circle
    
    * config circle
    
    * config circle
    
    * config circle
    
    * config circle
    
    * config circle
    
    * config circle
    
    * add S3 deploy
    
    * test
    
    * test
    
    * test
    
    * test
    
    * test
    
    * add serverless
    
    * add Prod deploy capacity
    
    * add Prod deploy capacity

---
+++ .circleci/deploy.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -o nounset
+set -o errexit
+
+
+current_build="${DEPLOY_FUNCTION}"
+
+zip -r "${current_build}.zip" *.js config node_modules
+echo "Checking if function $current_build already exists"
+aws lambda list-functions | jq -r --arg CURRENTFUNCTION "$current_build" '.Functions[] | select(.FunctionName==$CURRENTFUNCTION) | .FunctionArn'
+echo "Sending zip to S3"
+aws s3api put-object --bucket alhau --key "deploy/${current_build}.zip" --body "${current_build}.zip"
+echo "Updating function: $current_build"
+aws lambda update-function-code --function-name "$current_build" --s3-bucket alhau --s3-key "deploy/${current_build}.zip" --region eu-west-1 --no-publish
